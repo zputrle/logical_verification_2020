1230b1258030b842dcfd49111a56ebc5f47fbffd
zputrle <ziga.putrle@zputrle.com>
2021-08-10 11:34:38 +0200

    Finished 5th homework

--- lean/love05_inductive_predicates_homework_sheet.lean
+++ lean/love05_inductive_predicates_homework_sheet.lean
@@ -26,10 +26,21 @@ and only if its argument has an `app` constructor at the top level. -/
 
 -- enter your definition here
 
+inductive is_app : term -> Prop
+| app (t1 t2: term): is_app (term.app t1 t2)
+
 /- 1.2 (2 points). Define an inductive predicate `is_abs_free` that is true if
 and only if its argument is a λ-term that contains no λ-expressions. -/
 
 -- enter your definition here
+
+inductive is_app_free' : term -> Prop
+| var (s: string): is_app_free' (term.var s)
+| app (s: string) (t: term): is_app_free' t -> is_app_free' (term.lam s t)
+
+inductive is_app_free : term -> Prop
+| app (t1 t2: term):
+  is_app_free' t1 -> is_app_free' t2 -> is_app_free (term.app t1 t2)
 
 
 /- ## Question 2 (6 points): Even and Odd
@@ -45,18 +56,19 @@ Lean definition below. The definition should distinguish two cases, like `even`,
 and should not rely on `even`. -/
 
 inductive odd : ℕ → Prop
--- supply the missing cases here
+| one : odd 1
+| add_two {n : ℕ} : odd n -> odd (n + 2)
 
 /- 2.2 (1 point). Give proof terms for the following propositions, based on
 your answer to question 2.1. -/
 
 lemma odd_3 :
   odd 3 :=
-sorry
+odd.add_two odd.one
 
 lemma odd_5 :
   odd 5 :=
-sorry
+odd.add_two odd_3
 
 /- 2.3 (2 points). Prove the following lemma by rule induction, as a "paper"
 proof. This is a good exercise to develop a deeper understanding of how rule
@@ -83,18 +95,84 @@ function definition or an introduction rule for an inductive predicate. -/
 
 -- enter your paper proof here
 
+/-
+lemma even_odd: ∀n: ℕ, even n -> odd (n + 1)
+
+Proof:
+  For some n, assume that n is even, we derive odd (n + 1) by
+  induciton on even:
+
+  (Base case) We assume even n = even 0 = even.zero. Then
+    odd (0 + 1) = odd 1. One is odd by definition of odd (i.e. by odd.one).
+
+  (Inductive step) IH: We assume even n -> odd (n + 1). Then
+    even.add_two n -> odd.add_two (n + 1) =
+      (by definition of even.add_two and odd.add_two)
+    even (n + 2) -> odd (n + 3).
+      By definition odd we know that odd (n + 3) only holds if odd (n + 1)
+      holds. We assume that even (n + 2) hold. Therfore, by def. of even, even n
+      also holds. Thus, by IH we know that odd (n + 1) hold and therfore
+      odd (n + 3).
+-/
+
 /- 2.4 (1 point). Prove the same lemma again, but this time in Lean: -/
 
 lemma even_odd {n : ℕ} (heven : even n) :
   odd (n + 1) :=
-sorry
+begin
+  induction' heven,
+    {simp, exact odd.one},
+    {exact odd.add_two ih,}
+end
 
 /- 2.5 (1 point). Prove the following lemma in Lean.
 
 Hint: Recall that `¬ a` is defined as `a → false`. -/
 
+#check even_odd
+
+lemma not_two_odd_in_row : ∀n : ℕ, odd n -> ¬ odd (n + 1) :=
+begin
+  intro n,
+  induction' n,
+  {rw not_def,
+   assume h0,
+   cases h0,},
+  {intro h1,
+   cases h1,
+   {simp at ih,
+    rw not_def,
+    intro,
+    cases a,
+    have hn := ih a_a,
+    have contra := and.intro hn h1,
+    simp at contra,
+    exact contra},
+   {intro h,
+    cases h,
+    have hn := ih h_a,
+    have contra := and.intro h1 hn,
+    simp at contra,
+    exact contra}}
+end
+
 lemma even_not_odd {n : ℕ} (heven : even n) :
   ¬ odd n :=
-sorry
+begin
+  rw not_def,
+  have h : odd (n+1) := even_odd heven,
+  intro hn,
+  have hn := not_two_odd_in_row n hn,
+  have contra := and.intro h hn,
+  simp at contra,
+  exact contra,
+end
+
+-- lemma even_not_odd {n : ℕ} (heven : even n) :
+--   ¬ odd n :=
+-- begin
+--   rw not_def,
+--   have h : odd (n+1) := even_odd heven,
+-- end
 
 end LoVe
